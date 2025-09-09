3bb3408772f76491b0c4d929c0b8b068446ab798
enggelin <enggelingiacinta@gmail.com>
2025-09-09 16:18:03 +1200

    Modified the tsamp.cpp function so the variable x doesn't leak to the GlobalEnv

--- src/tsamp.cpp
+++ src/tsamp.cpp
@@ -3,20 +3,21 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 NumericVector tsamp(std::string expr, NumericVector x, Environment env) {
-  Function parse("parse");
-  SEXP expr_vec = parse(Named("text") = expr);
+  Function parse("parse"), evalF("eval");
+
+  std::string fn_text = std::string("function(x) ") + expr;
+  SEXP expr_vec = parse(Named("text") = fn_text);
   if (Rf_length(expr_vec) < 1){
     stop("Failed to parse expression text.");
   }
   SEXP expr1 = VECTOR_ELT(expr_vec, 0);
+  Function fun = as<Function>(evalF(expr1, env));
 
   R_xlen_t n = x.size();
   NumericVector out(n);
 
   for (R_xlen_t i = 0; i < n; ++i){
-    env.assign("x", x[i]);
-    SEXP val = Rf_eval(expr1, env);
-    out[i] = as<double>(val);
+    out[i] = as<double>(fun(x[i]));
   }
   return out;
 }
